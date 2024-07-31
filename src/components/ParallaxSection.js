09e1053ce98dc1018775dc76f47112ea0ee51d70
Jotagamaral <jota.amaralmiranda@gmail.com>
2024-07-31 15:55:15 -0300

    Video Welcome add adn Midia changes

--- src/components/ParallaxSection.js
+++ src/components/ParallaxSection.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
-const ParallaxSection = ({ backgroundImage, children, height = 'h-screen', color, imageOpacity = 0.5 }) => {
+const ParallaxSection = ({ backgroundImage, backgroundVideo, children, height = 'h-screen', color, mediaOpacity = 0.5 }) => {
   return (
     <div className={`relative ${height}`}>
       <div className={`absolute inset-0 ${color} z-0`}></div>
       <div className="relative w-full h-full">
-        <div 
-          className="absolute inset-0 bg-fixed bg-center bg-cover" 
-          style={{ backgroundImage: `url(${backgroundImage})`, opacity: imageOpacity }}>
-        </div>
+        {backgroundVideo ? (
+          <video 
+            className="absolute inset-0 w-full h-full object-cover"
+            style={{ opacity: mediaOpacity }}
+            autoPlay
+            loop
+            muted
+          >
+            <source src={backgroundVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <div 
+            className="absolute inset-0 bg-fixed bg-center bg-cover" 
+            style={{ backgroundImage: `url(${backgroundImage})`, opacity: mediaOpacity }}
+          ></div>
+        )}
         <div className="relative bg-opacity-50 h-full flex items-center z-10">
           <div className="w-full p-10 text-white">
             {children}
@@ -20,4 +33,3 @@ const ParallaxSection = ({ backgroundImage, children, height = 'h-screen', color
 };
 
 export default ParallaxSection;
-
