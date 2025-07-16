a328b72e2fea3595d2b8229dcb9858da7e27583b
João Gabriel <jota.amaralmiranda@gmail.com>
2025-07-16 17:58:56 -0300

    Change photos & fix layout Conselho Pastoral

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,14 @@ import Welcome from './Welcome';
 import Jesus from './Jesus';
 import Cultos from './Cultos';
 import Memorial from './Memorial';
-import Popup from '../components/Popup';  // Importe o novo componente
+// import Popup from '../components/Popup';  // Importe o novo componente
 
 const Home = ({ refs }) => {
   const { jesusRef, memorialRef, cultosRef } = refs;
 
   return (
     <div>
-      <Popup /> {/* Adicione o componente Popup */}
+      {/*<Popup />} {/* Adicione o componente Popup */}
       <div id="home">
         <Welcome cultosRef={cultosRef} />
       </div>
