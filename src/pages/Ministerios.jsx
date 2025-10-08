d7729d8a1c12bb49de70958fdfa9d4fa60af9d1a
João Gabriel <jota.amaralmiranda@gmail.com>
2025-10-08 15:49:59 -0300

    Changes in Ministerios link button

--- src/pages/Ministerios.jsx
+++ src/pages/Ministerios.jsx
@@ -53,7 +53,7 @@ const Ministerios = () => {
               <h2 className="text-5xl md:text-6xl font-bold text-custom-blue font-arya">VOLUNTÁRIO?</h2>
             </div>
             <a
-              href="https://form.jotform.com/230955606462661"
+              href="https://form.jotform.com/251174825458059"
               target="_blank"
               rel="noopener noreferrer"
               className="px-6 py-2 md:px-8 md:py-2 bg-custom-blue text-white text-lg font-poppins font-bold rounded-3xl hover:bg-blue-900 transition duration-300 inline-block"
